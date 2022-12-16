794c013384bcf3a7648f21c9a7ae93d7ffd10584
Lassi Kirvesmäki <lassi.kirvesmaki@tuni.fi>
2022-12-16 14:30:10 +0200

    Test cases for isEmpty.js

---
+++ src/isEmpty.test.js
@@ -0,0 +1,22 @@
+import isEmpty from './isEmpty'
+
+describe('isEmpty', () => {
+    it('Should return true with input of null', () => {
+        expect(isEmpty(null)).toStrictEqual(true);
+    })
+    it('Should return true with input of true', () => {
+        expect(isEmpty(true)).toStrictEqual(true);
+    })
+    it('Should return true with input of 1', () => {
+        expect(isEmpty(1)).toStrictEqual(true);
+    })
+    it('Should return false with input of [1, 2, 3]', () => {
+        expect(isEmpty([1, 2, 3])).toStrictEqual(false);
+    })
+    it('Should return false with input of abc', () => {
+        expect(isEmpty('abc')).toStrictEqual(false);
+    })
+    it('Should return false with input of { a : 1 }', () => {
+        expect(isEmpty({ 'a' : 1 })).toStrictEqual(false);
+    })
+})
